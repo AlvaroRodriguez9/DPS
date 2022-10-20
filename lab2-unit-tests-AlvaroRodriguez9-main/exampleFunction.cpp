468042893d0b5cb2ee0f732465ba649ed5f1d50d
AlvaroRodriguez9 <114727437+AlvaroRodriguez9@users.noreply.github.com>
2022-10-20 12:21:27 +0200

    Update exampleFunction.cpp

--- lab2-unit-tests-AlvaroRodriguez9-main/exampleFunction.cpp
+++ lab2-unit-tests-AlvaroRodriguez9-main/exampleFunction.cpp
@@ -3,18 +3,35 @@
 #include <stdio.h>
 #include <stdint.h> 
 
+
 unsigned int wrapFunctionAdd(unsigned int ui_a, unsigned int ui_b) {
-  unsigned int usum = ui_a + ui_b;
+  unsigned int usum; 
+  if (UINT_MAX - ui_a < ui_b){
+    printf("Los números introducidos producen wrap\n");
+    return 0;
+  } else {
+    usum = ui_a + ui_b;
+  }
   return usum;
 }
 
 unsigned int wrapFunctionMul(unsigned int ui_a, unsigned int ui_b) {
   unsigned umul = ui_a;
-  umul *= ui_b;
+  if (ui_a > UINT_MAX / ui_b){
+    printf("Los números introducidos producen wrap\n");
+    return 0;
+  } else {
+    umul *= ui_b;
+  }
   return umul;
 }
 
 uint32_t wrapFunctionShift(uint32_t ui_a, unsigned int ui_b) {
-  uint32_t uShift = ui_a << ui_b | ui_a >> (32 - ui_b); 
+  uint32_t uShift;
+  if (ui_b >= UINT_MAX){
+    printf("Los números introducidos producen wrap\n");
+  } else {
+    uShift = ui_a << ui_b | ui_a >> (32 - ui_b);
+  } 
   return uShift;
 }
